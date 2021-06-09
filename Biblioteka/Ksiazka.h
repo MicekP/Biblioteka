105a4348fc03d6fe162ac406cce8906312ab8d8b
lisakm-agh <85515588+lisakm-agh@users.noreply.github.com>
2021-06-09 18:02:41 +0200

    Update Ksiazka.h

--- Biblioteka/Ksiazka.h
+++ Biblioteka/Ksiazka.h
@@ -4,13 +4,36 @@
 #include <iostream>
 using namespace std;
 
-class Ksiazka {
+class Ksiazka {	
 public:
 	int ID;
 	string tytul;
 	string autor;
 	int L_egz;						//liczba wszystkich egzemplarzy
-	int L_wolnych_egz;				//liczba egzemplarzy w bibliotece, niewypozyczonych
+	int L_wolnych_egz;			//liczba egzemplarzy w bibliotece, niewypozyczonych
+	
+	Ksiazka()
+	{
+		ID = 0;
+		Tytul = "X";
+		Autor = "Y Z";
+		
+		L_egz = 0;
+		L_wolnych_egz = 0;
+	}
+	
+	Ksiazka(int i, string t, string a)
+	{
+		ID = i;
+		Tytul = t;
+		Autor = a;
+		
+		L_egz = 1;
+		L_wolnych_egz = 1;
+	}
+	
+	~Ksiazka() {};
+		
 
 	void Dodaj(string, string, int);				// dodawanie pozycji, tytul, autor, ilosc
 	void Usun();								//usuniecie calej pozycji, gdy nie ma juz w ogole egzemplarzy
