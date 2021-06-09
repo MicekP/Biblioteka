00a49e29f98ea6f65c6a2f206bb31cacfdd51763
lisakm-agh <lisakm@student.agh.edu.pl>
2021-06-09 18:45:59 +0200

    ksiazka+

--- Biblioteka/Ksiazka.cpp
+++ Biblioteka/Ksiazka.cpp
@@ -31,38 +31,118 @@ void Usun()
 
 void Dodaj_egzemplarz_id()
 {
-  int X;
-  int Y;
+    int X;
+    int Y;
   
-  cout << "Podaj ID ksiazki:" << endl;
+    cout << "Podaj ID ksiazki:" << endl;
 	cin >> X;
-  cout << "Podaj ilosc egzemplarzy do dodania:" << endl;
-	cin >> Y;
+    cout << "Podaj ilosc egzemplarzy do dodania:" << endl;
+    cin >> Y;
   
-  for(int i; i < 10; i++)
-  {
-   if(tab_ksia[i].ID == X)
-   {
-      tab_ksia[i].L_egz += Y;
-      tab_ksia[i].L_wolnych_egz += Y;
-      return;
-   }
-   else
-   {
-      cout << "Brak ksiazki o podanym ID" << endl;
-      return;
-   }
-  }
+    for(int i; i < 10; i++)
+    {
+        if(tab_ksia[i].ID == X)
+        {
+            tab_ksia[i].L_egz += Y;
+            tab_ksia[i].L_wolnych_egz += Y;
+            return;
+        }
+        else
+        {
+            cout << "Brak ksiazki o podanym ID" << endl;
+            return;
+        }
+    }
 }
 void Dodaj_egzemplarz()
 {
-  
+    string X;
+    string Y;
+    int Z;
+
+    cout << "Podaj tytul ksiazki:" << endl;
+    cin >> X;
+    cout << "Podaj autora:" << endl;
+    cin >> Y;
+    cout << "Podaj ilosc egzemplarzy do dodania:" << endl;
+    cin >> Z;
+
+    for (int i; i < 10; i++)
+    {
+        if (tab_ksia[i].tytul == X && tab_ksia[i].autor == Y)
+        {
+            tab_ksia[i].L_egz += Z;
+            tab_ksia[i].L_wolnych_egz += Z;
+            return;
+        }
+        else
+        {
+            cout << "Brak ksiazki o podanym tytule i/lub autorze" << endl;
+            return;
+        }
+    }
+    
 }
 void Usun_egzemplarz_id()
 {
-  
+    int X;
+    int Y;
+
+    cout << "Podaj ID ksiazki:" << endl;
+    cin >> X;
+    cout << "Podaj ilosc egzemplarzy do usuniecia:" << endl;
+    cin >> Y;
+
+    for (int i; i < 10; i++)
+    {
+        if (tab_ksia[i].ID == X)
+        {
+            tab_ksia[i].L_egz -= Y;
+            tab_ksia[i].L_wolnych_egz -= Y;
+            
+            if (tab_ksia[i].L_egz <= 0)
+            {
+                //Usun();
+            }
+            return;
+        }
+        else
+        {
+            cout << "Brak ksiazki o podanym ID" << endl;
+            return;
+        }
+    }
 }
 void Usun_egzemplarz()
 {
-  
+    string X;
+    string Y;
+    int Z;
+
+    cout << "Podaj tytul ksiazki:" << endl;
+    cin >> X;
+    cout << "Podaj autora:" << endl;
+    cin >> Y;
+    cout << "Podaj ilosc egzemplarzy do usuniecia:" << endl;
+    cin >> Z;
+
+    for (int i; i < 10; i++)
+    {
+        if (tab_ksia[i].tytul == X && tab_ksia[i].autor == Y)
+        {
+            tab_ksia[i].L_egz -= Z;
+            tab_ksia[i].L_wolnych_egz -= Z;
+
+            if (tab_ksia[i].L_egz <= 0)
+            {
+                //Usun();
+            }
+            return;
+        }
+        else
+        {
+            cout << "Brak ksiazki o podanym tytule i/lub autorze" << endl;
+            return;
+        }
+    }
 }
